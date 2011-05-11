fbb84f0ac40436da9734f95fb1a72042e99eee31
pgbennett <pgbennett@comcast.net>
2011-05-11 22:35:35 +0000

    Set up debian unstable build

--- ubuntu_build_source.sh
+++ ubuntu_build_source.sh
@@ -6,9 +6,11 @@ scriptname=`readlink -e "$0"`
 scriptpath=`dirname "$scriptname"`
 
 cd $scriptpath
-
+echo optional parameter list of distros
+list="$1"
 
 export VERSION=`cat VERSION`
+mkdir -p package
 if [[ ! -f package/PPAVERSION ]] ; then
     echo 1 > package/PPAVERSION
     ppaversion=1
@@ -17,26 +19,45 @@ else
     echo $ppaversion > package/PPAVERSION
 fi
 echo PPA Version $ppaversion
-echo press enter if ok, ctrl-c if not
-read xxx
+echo press enter if ok, or enter value
+read -e xxxin
+if [[ "$xxxin" != "" ]] ; then
+    ppaversion=$xxxin
+    echo $ppaversion > package/PPAVERSION
+fi
 make source
 mkdir -p ~/proj/local/jampal/jampal-$VERSION
 cp package/source/jampal-$VERSION.tar.gz ~/proj/local/jampal/
 cd ~/proj/local/jampal/jampal-$VERSION
 rm -rf ../sourcepackage
-for distro in lucid maverick natty oneiric ; do
+versionsuffix="-"
+if [[ "$list" = "" ]] ; then
+    list="unstable lucid maverick natty oneiric"
+fi
+for distro in $list ; do
+    if [[ "$distro" == unstable ]] ; then
+        distrosuffix=
+        versionsuffix="-"
+        reason=debian
+    else
+        versionsuffix=ubuntu1ppa
+        distrosuffix="~$distro"
+        reason=ppa
+    fi
     rm -rf *
     rm -rf ../jampal_$VERSION*
     tar xf ../jampal-$VERSION.tar.gz
     cd debian
-    echo "jampal (${VERSION}ubuntu1ppa`printf %02d $ppaversion`~$distro) $distro; urgency=low" > changelogxxx
+    echo "jampal (${VERSION}${versionsuffix}`printf %02d $ppaversion`$distrosuffix) $distro; urgency=low" > changelogxxx
     echo >> changelogxxx
-    echo "  * Build for ppa" >> changelogxxx
-    echo " -- Peter Bennett <pgbennett@users.sourceforge.net>  `date -R`" >> changelogxxx
+    echo "  * Build for $reason" >> changelogxxx
+    echo >> changelogxxx
+    echo " -- Peter Bennett <pgbennett@comcast.net>  `date -R`" >> changelogxxx
+    echo >> changelogxxx
     cat changelog >> changelogxxx
     mv -f changelogxxx changelog
     cd ..
-    echo build PPA Version $ppaversion for $distro
+    echo build Suffix Version $ppaversion for $distro
     dpkg-buildpackage -S
     dirname=../sourcepackage/$distro
     mkdir -p $dirname
